20164a05136b800298e1de9bb976baf92f07eeb0
Ankush Singh Gandhi <bhanusinghank@gmail.com.com>
2021-08-10 19:32:40 +0530

    initialized flutter project

---
+++ android/app/src/main/java/com/ankushgandhi/calculator_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ankushgandhi.calculator_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('HI'),
+      ),
+    );
+  }
+}
